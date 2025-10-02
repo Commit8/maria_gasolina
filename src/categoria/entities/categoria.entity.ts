6343cc18bad2fc2f58780daea0af518c863c93a5
pe3dru <lionpedro30@gmail.com>
2025-10-02 13:29:20 -0300

    categoria.entity e usuario.module

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Corrida } from '../../corrida/entities/corrida.entity';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  veiculo: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  preco: number;
+
+  @OneToMany(() => Corrida, (corrida) => corrida.categoria)
+  categoria: Categoria[];
+}
